ce3e4d94dbceda52f18ee8db85c0e1fb3ef73ec2
PatrunoLuca <patruno.luca.s26@liceoviconapoli.it>
2023-01-22 15:05:47 +0100

    Bug fix

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ def main_function():
 
     # Initialise the CSV file
     base_folder = Path(__file__).parent.resolve()
-    data_file = str(base_folder) + '/data.csv'
+    data_file = base_folder / "data.csv"
     create_csv(data_file)
 
     # Inizialise the Log File
-    logfile(base_folder/"events.log")
+    logfile(base_folder / "events.log")
 
     # Collecting current time
     start_time = datetime.now()
@@ -32,48 +32,49 @@ def main_function():
     loop = 0
 
     # Run loop for three hours
-    while (now_time < start_time + timedelta(minutes=180)):
+    while now_time < start_time + timedelta(minutes=180):
 
         # Save each lap in log
-        logger.info(f'Loop number {loop+1} started')
+        logger.info(f"Loop number {loop+1} started")
 
         # Variables for the dayNight function
-        timescale = load.timescale().now()
+        timescale = load.timescale().now()  # Unused
         # Load ephemeris (high accuracy table with position of celestial objects)
-        ephemeris = load('de421.bsp')
+        ephemeris = load("de421.bsp")  # Unused
+
         light = dayNight()
 
         # If the ISS is orbiting above the illuminated part of the earth run this code
         if light == True:
-            logger.info('day - save image')
+            logger.info("day - save image")
 
             # Determine the path and name of the images
             image_name = str(datetime.now().strftime("%Y%m%d-%H%M%S"))
-            path_image = str(base_folder) + '/images/' + image_name             
-            
+            path_image = str(base_folder) + "/images/" + image_name
+
             # Capturing the images
-            try: 
-                camera.capture(f"{save_file}")
-                #capture(path_image, data_file, 0)
+            try:
+                capture(path_image, data_file, 0)
+                # camera.capture(f"{save_file}")
             except Exception as e:
-                logger.error(f'{e.__class__.__name__}: {e}')
-            
+                logger.error(f"{e.__class__.__name__}: {e}")
+
         else:
-            logger.info('night - wait 20 seconds')
+            logger.info("night - wait 20 seconds")
             sleep(20)
 
         # Update the current time
         now_time = datetime.now()
         print(now_time)
-        
+
         # Raspberry warm-up time in order to avoid thermal-throttling
         sleep(9.5)
-        
+
         loop += 1
 
 
 # Main code
-if __name__ == '__main__':
-    print('main.py - AstroPI 2021/2022')
+if __name__ == "__main__":
+    print("main.py - AstroPI 2022/2023")
 
     main_function()
